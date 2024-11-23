a22fe80f5f2d34ec6031b9a8d9c47f5458952a3d
Nik Uzair <nikuzairsc@gmail.com>
2024-11-23 17:50:21 +0800

    revise code

--- QuizAppSnapshotTests/QuizAppSnapshotTests.swift
+++ QuizAppSnapshotTests/QuizAppSnapshotTests.swift
@@ -8,25 +8,11 @@
 import XCTest
 import SnapshotTesting
 @testable import QuizApp
-import SwiftUI
 
 final class QuizAppSnapshotTests: XCTestCase {
   
-  private var screenWidth: CGFloat {
-    return UIScreen.main.bounds.size.width
-  }
-  
   func testQuizListScreen() {
-      let quiz = QuizData.loadQuizes()[0]
-      let quizSubmission = QuizSubmission(quizId: quiz.quizId)
-      
-      let view = QuestionListScreen(quiz: quiz, quizSubmission: quizSubmission)
-      
-      let hostingController = UIHostingController(rootView: view)
-      
-      let size = CGSize(width: screenWidth, height: 48)
-      hostingController.view.frame = CGRect(origin: .zero, size: size)
-      
-      assertSnapshot(of: hostingController.view, as: .image(size: size), record: true)
-    }
+    let view = QuizListScreen()
+    assertSnapshot(of: view, as: .image)
+  }
 }
