84d484dc633c8357a88f72cfb9ab140c6a3a6220
Nik Uzair <nikuzairsc@gmail.com>
2024-11-23 18:31:11 +0800

    failing build code in purpose

--- QuizApp/Screens/QuestionListScreen.swift
+++ QuizApp/Screens/QuestionListScreen.swift
@@ -26,7 +26,7 @@ struct QuestionListScreen: View {
                         let question = quiz.questions[index]
                         
                         VStack(alignment: .leading) {
-                            HStack {
+                            HStac {
                                 Text("\(index + 1)")
                                     .padding(10)
                                     .foregroundColor(.white)
